e8c8e4c6790bca1996fdeb26695f63043bf80a6c
Juan Cremades <relicary@gmail.com>
2025-02-28 14:49:01 +0100

    ROUTE: Trending and Search are Children Routes of Dashboard. Both components are inside Dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,21 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'dashboard',
-    loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page.component')
-  },
-  {
-    path: 'trending',
-    loadComponent: () => import('./gifs/pages/trending-page/trending-page.component')
-  },
-  {
-    path: 'search',
-    loadComponent: () => import('./gifs/pages/search-page/search-page.component')
+    loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page.component'),
+    children: [
+      {
+        path: 'trending',
+        loadComponent: () => import('./gifs/pages/trending-page/trending-page.component')
+      },
+      {
+        path: 'search',
+        loadComponent: () => import('./gifs/pages/search-page/search-page.component')
+      },
+      {
+        path: '**',
+        redirectTo: 'trending'
+      }
+    ]
   },
   {
     path: '**',
