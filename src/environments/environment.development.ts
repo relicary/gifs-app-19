9253c4eed7b5aa58e9dc1c2cf95f1cd1017137ed
Juan Cremades <relicary@gmail.com>
2025-03-03 10:53:33 +0100

    ENVIRONMENT: Added envitonment using ng g environments command

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'Apps',
+  companySlogan: 'Manage your Gifs',
+
+  // ApiKeys
+
+  //URLs
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifs',
+  companySlogan: 'Manage your Gifs',
+
+  // ApiKeys
+
+  //URLs
+};
