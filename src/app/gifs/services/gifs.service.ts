64510ce0a504bd16f2376a66798f8e631093b370
Juan Cremades <relicary@gmail.com>
2025-03-04 15:59:22 +0100

    SERVICES: Trending Gifs are rendered into a Mansory view element

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -23,6 +23,18 @@ export class GifService {
   trendingGifs = signal<Gif[]>([]);
   trendingGifsLoading: WritableSignal<boolean> = signal(true);
 
+  trendingGifGroup = computed<Gif[][]>( () => {
+    const groups = [];
+
+    for( let i = 0; i < this.trendingGifs().length; i+=3) {
+      groups.push(this.trendingGifs().slice(i , i+3));
+    }
+
+    console.log({groups})
+
+    return groups;
+  });
+
   searchHistory = signal<Record<string, Gif[]>>(loadFromLocalStorage());
   searchHistoryKeys = computed(() =>
     Object.keys(this.searchHistory())
